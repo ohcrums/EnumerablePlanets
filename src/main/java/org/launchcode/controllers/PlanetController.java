03a67ad9b4be7566573644ad52d85de03cceb7d3
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-23 15:09:10 -0500

    step
    controller passes Planet values to model

--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -1,13 +1,17 @@
 package org.launchcode.controllers;
 
+import org.launchcode.data.Planets;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PlanetController {
 
-    @GetMapping
-    public String displayIndex() {
+    @GetMapping("")
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
