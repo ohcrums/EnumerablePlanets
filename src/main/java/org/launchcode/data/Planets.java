5e25b2488136c66662183c2fe82a5f05d87fda81
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-24 08:56:12 -0500

    done

--- src/main/java/org/launchcode/data/Planets.java
+++ src/main/java/org/launchcode/data/Planets.java
@@ -2,14 +2,36 @@ package org.launchcode.data;
 
 public enum Planets {
 
-    MERCURY,
-    VENUS,
-    EARTH,
-    MARS,
-    JUPITER,
-    SATURN,
-    URANUS,
-    NEPTUNE,
-    PLUTO
 
+    MERCURY("Mercury", 88, 0),
+    VENUS("Venus", 225, 0),
+    EARTH("Earth", 365, 1),
+    MARS("Mars", 687, 2),
+    JUPITER("Jupiter", 4333, 95),
+    SATURN("Saturn", 10759, 146),
+    URANUS("Uranus", 30687, 28),
+    NEPTUNE("Neptune", 60200, 16),
+    PLUTO("Pluto", 90520, 5);
+
+    private final String name;
+    private final int yearLength;
+    private final int moons;
+
+    Planets(String name, int yearLength, int moons) {
+        this.name = name;
+        this.yearLength = yearLength;
+        this.moons = moons;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearLength() {
+        return yearLength;
+    }
+
+    public int getMoons() {
+        return moons;
+    }
 }
